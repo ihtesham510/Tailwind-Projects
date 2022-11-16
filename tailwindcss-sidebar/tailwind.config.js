296ce704a5b8071785d0b96f197712d5f45fa43b
ihtesham <ihteshamulhaq510@gmail.com>
2022-11-17 00:30:10 +0500

    .

---
+++ tailwindcss-sidebar/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "sans":["poppins","Arial","sans-sarif" ]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
